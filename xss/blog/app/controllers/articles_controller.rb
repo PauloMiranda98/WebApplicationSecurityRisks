ae48cf7e2ee8c67ce975f80182f083c22cc84834
Paulo Miranda <paulomirandamss12@gmail.com>
2021-08-23 18:43:58 -0300

    fix bugs

--- xss/blog/app/controllers/articles_controller.rb
+++ xss/blog/app/controllers/articles_controller.rb
@@ -1,7 +1,4 @@
 class ArticlesController < ApplicationController
-
-#  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-
   def index
     @search = params[:search]
     if @search.present?
